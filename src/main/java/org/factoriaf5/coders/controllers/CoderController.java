0c321050001a6bb48a5da8b6bd4455c31ed4c055
desimorenoh <desimorenoh@gmail.com>
2022-01-06 15:20:17 +0100

    añado CRUD y plantilla all y edit

--- src/main/java/org/factoriaf5/coders/controllers/CoderController.java
+++ src/main/java/org/factoriaf5/coders/controllers/CoderController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -25,5 +28,32 @@ public class CoderController {
         model.addAttribute("title", "Lista de Coders");
         model.addAttribute("coders", coders);
         return "coders/all";
+    }
+
+    @GetMapping("/coders/new")
+    String newCoder(Model model) {
+    Coder coder = new Coder();
+    model.addAttribute("coder", coder);
+    model.addAttribute("title", "Crear nueva Coder");
+    return "coders/edit";
+    }
+
+    @PostMapping("/coders/new")
+    String addCoder(@ModelAttribute Coder coder) {
+        coderRepository.save(coder);
+        return "redirect:/coders";
+    }
+    @GetMapping("/coders/edit/{id}")
+    String editCoder(Model model, @PathVariable Long id){
+        Coder coder = coderRepository.findById(id).get();
+        model.addAttribute("coder", coder);
+        model.addAttribute("title", "Editar coder");
+        return "coders/edit";
+    }
+
+    @GetMapping("/coders/delete/{id}")
+    String remove(@PathVariable Long id) {
+        coderRepository.deleteById(id);
+        return "redirect:/coders";
     }
 }
--- src/test/java/org/factoriaf5/coders/ApplicationTests.java
+++ src/test/java/org/factoriaf5/coders/ApplicationTests.java
@@ -9,10 +9,15 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.Properties;
 
-import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
@@ -24,6 +29,7 @@ class ApplicationTests {
     void setUp() {
         coderRepository.deleteAll();
     }
+
     @Autowired
     MockMvc mockMvc;
 
@@ -48,4 +54,55 @@ class ApplicationTests {
                 .andExpect(model().attribute("coders", hasItem(coder)));
     }
 
+    @Test
+    void returnsAFormToAddANewCoder() throws Exception {
+        mockMvc.perform(get("/coders/new"))
+                .andExpect(view().name("coders/edit"))
+                .andExpect(model().attributeExists("coder"))
+                .andExpect(model().attribute("title", "Crear nueva Coder"));
+    }
+    @Test
+    void allowsToCreateANewCoder() throws Exception {
+        mockMvc.perform(post("/coders/new")
+                        .param("nombre", "Desirée")
+                        .param("apellidos", "Moreno")
+                        .param("edad", String.valueOf(42))
+                        .param("paisDeOrigen", "Venezuela")
+                        .param("nivelDeEstudios", "Técnico Superior")
+                        .param("dirección", "Calle Lletres 24, 1-2 Barcelona")
+                        .param("promoción", "Femtech P2")
+                )
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/coders"))
+        ;
+
+        List<Coder> existingCoders = (List<Coder>) coderRepository.findAll();
+        assertThat(existingCoders, contains(allOf(
+                hasProperty("nombre", equalTo("Desirée")),
+                hasProperty("apellidos", equalTo("Moreno")),
+                hasProperty("edad", equalTo(42)),
+                hasProperty("paisDeOrigen", equalTo("Venezuela")),
+                hasProperty("nivelDeEstudios", equalTo("Técnico Superior")),
+                hasProperty("dirección", equalTo("Calle Lletres 24, 1-2 Barcelona")),
+                hasProperty("promoción", equalTo("Femtech P2"))
+        )));
+    }
+    @Test
+    void returnsAFormToEditCoders() throws Exception {
+        Coder coder = coderRepository.save(new Coder("Desirée", "Moreno Hernández", 42, "Venezuela", "Técnico Superior", "Calle Lletres 24, 1-2 Barcelona", "Femtech P2"));
+        mockMvc.perform(get("/coders/edit/" + coder.getId()))
+                .andExpect(status().isOk())
+                .andExpect(view().name("coders/edit"))
+                .andExpect(model().attribute("coder", coder))
+                .andExpect(model().attribute("title", "Editar Coder"));
+    }
+    @Test
+    void allowsToDeleteACoder() throws Exception {
+        Coder coder = coderRepository.save(new Coder("Desirée", "Moreno Hernández", 42, "Venezuela", "Técnico Superior", "Calle Lletres 24, 1-2 Barcelona", "Femtech P2"));
+        mockMvc.perform(get("/coders/delete/" + coder.getId()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/coders"));
+
+        assertThat(coderRepository.findById(coder.getId()), equalTo(Optional.empty()));
+    }
 }
